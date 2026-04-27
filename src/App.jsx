ccef73e9d0e100632329bcca0dc4c4f015d54148
Kovács Dorina <kovacsdorina2@gmail.com>
2026-04-27 15:07:18 +0200

    Sidebar létrehozása

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import { useState } from "react"
 import Map from "./components/Map"
 import SavedPointsPanel from "./components/SavedPointsPanel"
+import Sidebar from "./components/Sidebar.jsx"
 
 function App() {
   const [points, setPoints] = useState([])
   const [selectedPoint, setSelectedPoint] = useState(null)
+  const [activePanel, setActivePanel] = useState(null)
 
   return (
     <div style={{ display: "flex", height: "100vh" }}>
-      <SavedPointsPanel
-        points={points}
-        onSelectPoint={setSelectedPoint}
-      />
+      <Sidebar activePanel={activePanel} setActivePanel={setActivePanel} />
+
+      {activePanel === "home" && (
+        <SavedPointsPanel
+          points={points}
+          onSelectPoint={setSelectedPoint}
+        />
+      )}
 
       <Map
         points={points}
