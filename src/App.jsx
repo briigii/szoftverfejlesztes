5e9b9d6e618779701745c59220246f606474c915
Kovács Dorina <kovacsdorina2@gmail.com>
2026-04-28 10:40:51 +0200

    settingPanel css létrehozása

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
 import { useState } from "react"
 import Map from "./components/Map"
+import Sidebar from "./components/Sidebar"
 import SavedPointsPanel from "./components/SavedPointsPanel"
-import Sidebar from "./components/Sidebar.jsx"
+import SettingsPanel from "./components/SettingsPanel"
 
 function App() {
   const [points, setPoints] = useState([])
   const [selectedPoint, setSelectedPoint] = useState(null)
   const [activePanel, setActivePanel] = useState(null)
 
+  const [user, setUser] = useState({
+    name: "Kovács Dorina",
+    email: "dorina@example.com",
+    username: "dorina",
+  })
+
   return (
     <div style={{ display: "flex", height: "100vh" }}>
       <Sidebar activePanel={activePanel} setActivePanel={setActivePanel} />
 
       {activePanel === "home" && (
-       <SavedPointsPanel
-        points={points}
-        selectedPoint={selectedPoint}
-        onSelectPoint={setSelectedPoint}
-        onBack={() => setSelectedPoint(null)}
-      />
+        <SavedPointsPanel
+          points={points}
+          selectedPoint={selectedPoint}
+          onSelectPoint={setSelectedPoint}
+          onBack={() => setSelectedPoint(null)}
+        />
       )}
 
-         {activePanel === "settings" && (
-       <SavedPointsPanel
-        points={points}
-        selectedPoint={selectedPoint}
-        onSelectPoint={setSelectedPoint}
-        onBack={() => setSelectedPoint(null)}
-      />
+      {activePanel === "settings" && (
+        <SettingsPanel user={user} setUser={setUser} />
       )}
 
       <Map
